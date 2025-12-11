7c60889311141fc140868c774ef53aeb35621f6e
liuyuyu <mikecool77882000@gmail.com>
2025-12-11 21:07:10 +0800

    echo command

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,18 @@
 #include <string>
 
 constexpr char EXIT_CMD[] = "exit";
+constexpr char ECHO_CMD[] = "echo";
+
+std::string get_command(const std::string_view arg) {
+    std::string command{};
+    if (auto first_space = arg.find(' '); first_space != std::string::npos) {
+        command = arg.substr(0, first_space);
+    } else {
+        command = arg;
+    }
+    return command;
+}
+
 int main() {
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
@@ -10,12 +22,19 @@ int main() {
   // Print prompt
   while (true) {
     std::cout << "$ ";
-    std::string command;
-    std::cin >> command;
-    if (command == "exit") {
+    std::string user_in;
+
+    std::getline(std::cin, user_in);
+    std::string command = get_command(user_in);
+
+    if (command == EXIT_CMD) {
         return 0;
+    } else if (command == ECHO_CMD) {
+        std::string arg = user_in.substr(command.length()+1);
+        std::cout << arg << "\n";
+    } else {
+        std::cout << user_in << ": command not found\n";
     }
-    std::cout << command << ": command not found\n";
   }
   return 0;
 }
