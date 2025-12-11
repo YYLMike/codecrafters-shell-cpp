3ca463055342aab1edfb42cb4a2aa9e6fe83a455
liuyuyu <mikecool77882000@gmail.com>
2025-12-11 21:16:15 +0800

    type command

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,14 @@
 
 constexpr char EXIT_CMD[] = "exit";
 constexpr char ECHO_CMD[] = "echo";
+constexpr char TYPE_CMD[] = "type";
+constexpr char ISBUILTIN_MSG[] = " is a shell builtin";
+constexpr char NOTFOUND_MSG[] = ": not found";
+constexpr char COMMANDNOTFOUND_MSG[] = ": command not found";
+
+bool is_builtin (const std::string_view arg) {
+    return  arg == EXIT_CMD || arg == ECHO_CMD || arg == TYPE_CMD;
+}
 
 std::string get_command(const std::string_view arg) {
     std::string command{};
@@ -26,14 +34,20 @@ int main() {
 
     std::getline(std::cin, user_in);
     std::string command = get_command(user_in);
+    std::string arg = user_in.substr(command.length()+1);
 
     if (command == EXIT_CMD) {
         return 0;
     } else if (command == ECHO_CMD) {
-        std::string arg = user_in.substr(command.length()+1);
         std::cout << arg << "\n";
+    } else if (command == TYPE_CMD) {
+        if (is_builtin(arg)) {
+            std::cout << arg << ISBUILTIN_MSG << "\n";
+        } else {
+            std::cout << arg << NOTFOUND_MSG << "\n";
+        }
     } else {
-        std::cout << user_in << ": command not found\n";
+        std::cout << user_in << COMMANDNOTFOUND_MSG << "\n";
     }
   }
   return 0;
